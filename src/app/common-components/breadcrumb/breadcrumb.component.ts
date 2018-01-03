38dd307144d83db5d1705efa94d74feb1c7cf635
VictorDeBlas <blasvan@gmail.com>
2018-01-03 11:44:49 +0100

    updated image

--- src/app/common-components/breadcrumb/breadcrumb.component.ts
+++ src/app/common-components/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'breadcrumb',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbComponent implements OnInit {
 
+  @Input() public currentSlide: number;
   constructor() { }
 
   ngOnInit() {
