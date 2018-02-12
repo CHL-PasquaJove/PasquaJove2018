4bb9a4d50f5a4974e02b0e2f9262e788dc533afa
Fernando Roman Garcia <fer.roman92@gmail.com>
2018-02-12 12:07:23 +0100

    Service improvements

--- src/app/api/user.service.ts
+++ src/app/api/user.service.ts
@@ -9,13 +9,14 @@ import { UserModel, ApiUser } from '../model/user.model';
 
 @Injectable()
 export class UserService {
-  constructor( private http: Http) { }
+  public baseUrl = 'https://api.pascuajoven.com'; // Should be on config
+  constructor(private http: Http) { }
 
   public newUser(user: UserModel): Observable<UserModel> {
-    const url = 'https://api.pascuajoven.com/api/newUser';
+    const method = 'newUser';
     const reqBody = user.toApi();
 
-    return this.doRequest(url, reqBody)
+    return this.doRequest(method, reqBody)
             .map((res) => {
               const resBody: ApiUser = res.json();
               return new UserModel().fromApi(resBody);
@@ -23,17 +24,17 @@ export class UserService {
   }
 
   public getUsers(): Observable<Array<UserModel>> {
-    const url = 'https://api.pascuajoven.com/api/getUsers';
+    const method = 'getUsers';
     const reqBody = {};
 
-    return this.doRequest(url, reqBody)
+    return this.doRequest(method, reqBody)
             .map((res) => {
               const body: ApiUser[] = res.json();
               return body.map( (u) => (new UserModel().fromApi(u)) );
             });
   }
 
-  private doRequest(url, reqBody) {
-    return this.http.post(url, reqBody);
+  private doRequest(method, reqBody) {
+    return this.http.post(`${this.baseUrl}/api/${method}`, reqBody);
   }
 }
