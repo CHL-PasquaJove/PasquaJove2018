bad7b491ee275e14ec35d8cef943b8289e7a46cc
Fernando Roman Garcia <fer.roman92@gmail.com>
2018-02-22 17:35:28 +0100

    Added messages table

--- src/app/responsible/messages/messages.component.ts
+++ src/app/responsible/messages/messages.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import * as moment from 'moment';
+
+import { ApiDate } from '../../model/api-date.model';
+import { ResponsibleFactory } from '../responsible/responsible.factory';
+
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss']
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent  {
+  @HostBinding('class.responsive-table') hasClass = true;
 
-  constructor() { }
+  constructor(public rf: ResponsibleFactory) { }
 
-  ngOnInit() {
+  public formatDate(date: ApiDate): string {
+    const years = moment().diff(date.val, 'years');
+    return `${years} | ${date.val.format('DD/MM/YYYY')}`;
   }
-
 }
